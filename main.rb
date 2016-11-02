5d6b571bc10f72dff6b79279d03c8defc84ed7d1
rachuxx <raszkiewicz.m@gmail.com>
2016-11-02 22:56:27 +0100

    Add input verification for Informer

--- main.rb
+++ main.rb
@@ -1,12 +1,17 @@
 require_relative 'constants.rb'
-test = Indexer.new
+start = Indexer.new
 
-puts 'Welcome to Concrete characteristics databe!'
-print "What class of concrete You want to look up for? (ex. C16/20, B37, etc.)\nClass: "
+puts 'Welcome to concrete characteristics database!'
+print 'What class of concrete You want to look up for? (ex. C16/20, B37, etc.)
+Class:'
 loop do
   str = gets.chomp
-  break if str.downcase == 'quit'
-  ix = test.index(str)
-  Loger.new.info(ix)
+  break if str.casecmp('quit').zero?
+  while start.index(str).nil?
+    print "\nThere's no such class! Try again!\nClass:"
+    str = gets.chomp
+  end
+  ix = start.index(str)
+  Informer.new.info(ix)
   puts "\nWant to chceck another? Which one? If no, type QUIT"
 end
